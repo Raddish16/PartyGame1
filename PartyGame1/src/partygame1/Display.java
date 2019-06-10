de1d248785728a6e74a93506bcb6817c37fba540
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-10 13:29:28 -0500

    Test

---
+++ PartyGame1/src/partygame1/Display.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partygame1;
+
+/**
+ *
+ * @author Yasuki Wu
+ */
+import javax.swing.*;
+import java.awt.*;
+
+public class Display {
+
+    private JFrame frame;
+    private Canvas canvas;
+
+    private String title;
+    private int width, height; //measuered in pixels
+    
+    public Display(String title, int width, int height)
+    {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay(){
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Ensures program stops properly
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null); //Sets location to center
+        frame.setVisible(true);
+        
+        canvas = new Canvas(); //Creates canvas object
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+        canvas.setFocusable(false);
+
+        frame.add(canvas);
+        frame.pack();
+        
+    }
+
+    public Canvas getCanvas()
+    {
+        return canvas;
+    }
+
+    public JFrame getFrame()
+    {
+        return frame;
+    } 
+}
+
---
+++ PartyGame1/src/partygame1/Game.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partygame1;
+
+import java.awt.image.BufferStrategy;
+
+/**
+ *
+ * @author Yasuki Wu
+ */
+public class Game implements Runnable{
+    private Display display;
+    private int width, height;
+    private String title;
+    private Thread thread;
+    private boolean running = false;
+private BufferStrategy bStrat;
+    public Game(String t, int w, int h){
+        title = t;
+        width = w;
+        height = h;
+    
+    }
+    
+    private void init(){
+        
+        
+    }
+    
+    private void tick(){
+        
+        
+    }
+    
+    private void render(){
+        
+        
+    }
+    
+    public void run(){
+        
+        
+    }
+    
+    public synchronized void start(){
+        
+        
+    }
+    
+    public synchronized void stop(){
+        
+        
+    }
+}
