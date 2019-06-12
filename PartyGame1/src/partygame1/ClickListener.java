1e37e4d611774b0e32f3ad2c60c2d1ea274eef68
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-12 18:38:06 -0500

    UI things wip

---
+++ PartyGame1/src/partygame1/ClickListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partygame1;
+
+/**
+ *
+ * @author Yasuki Wu
+ */
+public interface ClickListener {
+
+    public void onClick();
+}
