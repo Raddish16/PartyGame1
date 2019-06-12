b78139cbfbd6d284c98481cd9a5252ef3a3e695d
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-12 07:11:24 -0500

    Animation test with sprite sheet

--- PartyGame1/src/partygame1/Assets.java
+++ PartyGame1/src/partygame1/Assets.java
@@ -14,14 +14,20 @@ import java.util.ArrayList;
  */
 public class Assets {
     private static SpriteSheet sheet;
-    public static ArrayList<BufferedImage>mouse;
-    
+    public static ArrayList<BufferedImage>mouse;//easier to iterate through
+    public static BufferedImage[][]mouse2;//easier to reference
     public static void init(){
         sheet = new SpriteSheet(imageLoader.loadImage("/textures/mouseSheet.png"));
         mouse = new ArrayList<BufferedImage>();
         for(int y = 0; y<104;y+=26){
             for(int x = 0; x<104;x+=26){
                 mouse.add(sheet.crop(x, y, 26, 26));
+            }
+        }
+        mouse2 = new BufferedImage[4][4];
+        for(int c = 0; c<mouse2[0].length;c++){
+            for(int r = 0; r<mouse2.length;r++){
+                mouse2[r][c] = sheet.crop(r*26, c*26, 26, 26);
             }
         }
     }
