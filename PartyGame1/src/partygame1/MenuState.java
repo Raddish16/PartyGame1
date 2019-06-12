6b3c39c95c16b7916e4e1afb362da158f8fd261c
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-12 07:29:30 -0500

    Menustate

---
+++ PartyGame1/src/partygame1/MenuState.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partygame1;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Yasuki Wu
+ */
+public class MenuState extends State{
+    
+    public MenuState(Game game){
+        super(game);
+    }
+    public void tick(){
+        
+    }
+    public void render(Graphics graph){
+        
+    }
+}
