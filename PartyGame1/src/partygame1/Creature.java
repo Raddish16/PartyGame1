ef1a74ce24ff36c36dfc2eb50e0536ec57cc5a6e
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-12 18:02:21 -0500

    some stuff which will be helpful in minigames

--- PartyGame1/src/partygame1/Creature.java
+++ PartyGame1/src/partygame1/Creature.java
@@ -10,13 +10,39 @@ package partygame1;
  * @author brendanwilhelm
  */
 public class Creature extends Entity {
-
+    
+    public static final int defaultHealth = 10;
+    public static final float defaultSpeed = 3.0f;
+    public static final int defaultWidth = 32;
+    public static final int defaultHeight = 32;
+    protected int health;
+    protected float speed;
+    
+    
     public Creature(Handler handler, float x, float y, int width, int height) {
         super(handler, x, y, width, height);
+        health = defaultHealth;
+        speed = defaultSpeed;
     }
-
+    
     /*public void move(){
         x += xMove;
         y += yMove;
     }*/
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
 }
