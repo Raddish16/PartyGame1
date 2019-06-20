9a1c84e2bda03bbcd00b7f30e027047c89751ebc
Yasuki Wu <Yasuki Wu@LAPTOP-AGFV13BT>
2019-06-20 13:09:57 +0800

    no lag with the planes some new issues

--- PartyGame1/src/partygame1/Plane.java
+++ PartyGame1/src/partygame1/Plane.java
@@ -30,6 +30,9 @@ public class Plane extends Creature {
     public void move() {
         super.move();;
     }
+    public void resetCount(){
+        count = 0;
+    }
 
     public void tick() {
         count += .5;
--- PartyGame1/src/partygame1/PlaneGameState.java
+++ PartyGame1/src/partygame1/PlaneGameState.java
@@ -14,16 +14,17 @@ import java.util.ArrayList;
  * @author Yasuki Wu
  */
 public class PlaneGameState extends State {
-
+    
     private Handler handler;
     private Bomber bomber;
     private ArrayList<Plane> planeList;
     private ArrayList<PlaneGameTerrain> terrainList;
-    private int terrainSpawnCount;
-
+    private int planeNumber;//will hold the number of planes onscreen, will be scaled with time
+    
     public PlaneGameState(Handler handler) {
         super(handler);
         this.handler = handler;
+        planeNumber = 5;
         planeList = new ArrayList<Plane>();
         terrainList = new ArrayList<>();
         bomber = new Bomber(this.handler, 500, 500, 32, 32);
@@ -31,14 +32,14 @@ public class PlaneGameState extends State {
         planeList.add(new Plane(this.handler, 100, 500, 64, 32));
         planeList.add(new Plane(this.handler, 200, 500, 64, 32));
         terrainList.add(new PlaneGameTerrain(handler, (int) (Math.random() * (handler.getWidth())), 0, 64, 64));
-        terrainSpawnCount = 90;
         handler.setName("PlaneGameState");
         terrainSpawn();
+        spawn();
     }
-
+    
     public void terrainSpawn() {
-        for(int x = 0; x<12; x++){
-            terrainList.add(new PlaneGameTerrain(handler, (int)(Math.random()*handler.getWidth()),(int)(Math.random()*handler.getHeight()), 64,64));
+        for (int x = 0; x < 12; x++) {
+            terrainList.add(new PlaneGameTerrain(handler, (int) (Math.random() * handler.getWidth()), (int) (Math.random() * handler.getHeight()), 64, 64));
         }
         /*PlaneGameTerrain a, b, c;
         if (terrainSpawnCount > 90) {
@@ -56,50 +57,54 @@ public class PlaneGameState extends State {
             terrainSpawnCount = 0;
         }*/
     }
-
+    
     @Override
     public void tick() {
         
         bomber.tick();
-        for (int x = 0; x < planeList.size(); x++) {
-            planeList.get(x).tick();
-            if(planeList.get(x).getX() < 0 || planeList.get(x).getX() > handler.getWidth() || planeList.get(x).getY() < 0 || planeList.get(x).getY() > handler.getHeight()){
-                planeList.remove(x);
-            }
-        }
+        
         
         for (PlaneGameTerrain t : terrainList) {
             t.tick();
-            if(t.getY()>handler.getHeight()+20){
-                t.setX((int)(Math.random()*handler.getWidth()));
+            if (t.getY() > handler.getHeight() + 20) {
+                t.setX((int) (Math.random() * handler.getWidth()));
                 t.setY(-50);
             }
         }
-      //  spawn();
+        for (Plane p : planeList) {
+            p.tick();
+            if (p.getY() > handler.getHeight() +20 || p.getY()<-50 || p.getX() > handler.getWidth() +20|| p.getX() < -50) {
+                p.setX((int)(Math.random()*handler.getWidth()));
+                p.setY(-30);
+                p.resetCount();
+                System.out.println("ugh");
+            }
+        }
     }
-
+    
     @Override
     public void render(Graphics g) {
         g.setColor(new Color(113, 204, 65));
-        g.fillRect(0,0,handler.getWidth(),handler.getHeight());
+        g.fillRect(0, 0, handler.getWidth(), handler.getHeight());
         for (PlaneGameTerrain t : terrainList) {
             t.render(g);
         }
-
+        
         bomber.render(g);
-        for (int x = 0; x < planeList.size(); x++) {
-            planeList.get(x).render(g);
+        for (Plane p : planeList) {
+            p.render(g);
         }
     }
-
+    
     public void spawn() {
-        if (Math.random() > .95) {
-            if (Math.random() > .5) {
-                planeList.add(new Plane(this.handler, 0, (int) (Math.random() * 300), 64, 32));
-            }else{
-                planeList.add(new Plane(this.handler, 1400, (int) (Math.random() * 300), 64, 32));
+        for (int x = 0; x < planeNumber; x++) {
+            if (Math.random() > .95) {
+                if (Math.random() > .5) {
+                    planeList.add(new Plane(this.handler, 0, (int) (Math.random() * 300), 64, 64));
+                } else {
+                    planeList.add(new Plane(this.handler, 1400, (int) (Math.random() * 300), 64, 64));
+                }
             }
         }
-        
     }
 }
